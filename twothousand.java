83aba8e607f9e1ca5e81fa07efc2e0f51b10664f
chinmoy-paul <chinmoypaul8897@gmail.com>
2024-02-07 08:04:03 +0530

    second commit

--- twothousand.java
+++ twothousand.java
@@ -1,7 +1,11 @@
 package codechef.level500;
 
+import java.util.Scanner;
+
 public class twothousand {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // N - the number of notes of Rs. 2000
+        System.out.println((2000/500)*N);
     }
 }
